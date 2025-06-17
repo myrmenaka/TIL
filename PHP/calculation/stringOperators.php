b86f533e8daded300ea36ea952e9ec90f34b7995
akane <akamaru.0326@outlook.com>
2025-06-17 22:39:57 +0900

    practice

---
+++ PHP/calculation/stringOperators.php
@@ -0,0 +1,16 @@
+<?php
+    $first = "Hello";
+    $second = "World";
+    echo $first . " " . $second;
+
+    // " " は空文字（スペース）
+    // Hello World
+
+    $message = "りんご";
+    $message .= "ごりら";
+    $message .= "らっぱ";
+    $message .= "ぱいなっぷる";
+    echo $message;
+
+    // .= で変数の値に連結される
+    // りんごごりららっぱぱいなっぷる
