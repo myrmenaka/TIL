fff1f8a1b05276ced1d85c1a02471e86c141fc48
akane <akamaru.0326@outlook.com>
2025-06-19 17:03:57 +0900

    loop

---
+++ PHP/repetition/do-while.php
@@ -0,0 +1,13 @@
+<?php
+    $i = 1;
+
+    do {
+        echo "do-whileループ {$i}回目" . PHP_EOL;
+        $i++;
+    } while ($i <= 5);
+
+    // do-whileループ 1回目
+    // do-whileループ 2回目
+    // do-whileループ 3回目
+    // do-whileループ 4回目
+    // do-whileループ 5回目
---
+++ PHP/repetition/foreach.php
@@ -0,0 +1,12 @@
+<?php
+    $fruits = ["リンゴ", "バナナ", "みかん"];
+
+    // 配列$fruitsの要素を
+    // 最小インデックスから順に、$fruitの中に入れていく
+    foreach ($fruits as $fruit) {
+        echo $fruit . PHP_EOL;
+    };
+
+    // リンゴ
+    // バナナ
+    // みかん
