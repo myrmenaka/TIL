967e2d946aa67a3d3231753b2c8958ef62c3ee2b
akane <akamaru.0326@outlook.com>
2025-07-04 17:09:45 +0900

    DOCTYPE

--- PHP/form-5/form.php
+++ PHP/form-5/form.php
@@ -1,7 +1,19 @@
-<?php session_start(); ?>
-<form action="./confirm.php" method="post">
-    <p>名前: <input type="text" name="name"></p>
-    <p>メール: <input type="email" name="email"></p>
-    <input type="submit" value="確認画面へ">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>セッション</title>
+</head>
+<body>
+    
+    <?php session_start(); ?>
+    <form action="./confirm.php" method="post">
+        <p>名前: <input type="text" name="name"></p>
+        <p>メール: <input type="email" name="email"></p>
+        <input type="submit" value="確認画面へ">
+    </form>
 
+
+</body>
+</html>
--- PHP/security/form.php
+++ PHP/security/form.php
@@ -1,12 +1,24 @@
-<?php
-session_start();
-// トークン生成
-$token = bin2hex(random_bytes(32));
-$_SESSION['token'] = $token;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSRF</title>
+</head>
+<body>
+    
+    <?php
+    session_start();
+    // トークン生成
+    $token = bin2hex(random_bytes(32));
+    $_SESSION['token'] = $token;
+    ?>
 
-<form action="./confirm.php" method="post">
-    <p>名前: <input type="text" name="name"></p>
-    <input type="hidden" name="token" value="<?php echo htmlspecialchars($token, ENT_QUOTES, 'UTF-8'); ?>">
-    <input type="submit" value="送信">
-</form>
+    <form action="./confirm.php" method="post">
+        <p>名前: <input type="text" name="name"></p>
+        <input type="hidden" name="token" value="<?php echo htmlspecialchars($token, ENT_QUOTES, 'UTF-8'); ?>">
+        <input type="submit" value="送信">
+    </form>
+
+</body>
+</html>
