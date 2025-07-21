c0e6c212dfd6eb52dacc1fd14c0e6e039fd82802
akane <akamaru.0326@outlook.com>
2025-07-21 11:12:18 +0900

    MySQL

---
+++ PHP/MySQL-1/connect_test.php
@@ -0,0 +1,12 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=portfolio_db;charset=utf8mb4';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    echo "接続成功";
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage();
+}
+?>
