98466d27224b5aea127addb26745f3de9fd0747c
AB25 <AB25@PC127.fa.shiga-center.ac.jp>
2025-09-11 14:49:34 +0900

    commit

---
+++ Java-eclipse/account3/src/Account.java
@@ -0,0 +1,41 @@
+// 設計図クラス
+// 銀行口座クラス【第2版】
+
+class Account {
+	private String name;
+	private String no;
+	private long balance;
+	
+	// コンストラクタ(Accountクラスの初期化)
+	Account (String n, String num, long z) {
+		name = n;
+		no = num;
+		balance = z;
+	}
+	
+	// 口座名義を調べる
+	String getName() {
+		return name;
+	}
+	
+	// 口座番号を調べる
+	String getNo() {
+		return no;
+	}
+	
+	// 預金残高を調べる
+	long getBalance() {
+		return balance;
+	}
+	
+	// k円預ける
+	void deposit (long k) {
+		balance += k;
+	}
+	
+	// k円おろす
+	void withdraw (long k) {
+		balance -= k;
+	}
+	
+}
---
+++ Java-eclipse/account3/src/AccountTester3.java
@@ -0,0 +1,33 @@
+// 実行用クラス
+
+public class AccountTester3 {
+
+	public static void main(String[] args) {
+		// コンストラクタの呼出
+		// 足立さんの口座
+		Account adachi = new Account("足立幸一", "123456", 1000);
+		// 仲田さんの口座
+		Account nakata = new Account("仲田真二", "654321", 200);
+		
+		// 足立さんの口座から200円おろす
+		adachi.withdraw(200);
+		// 仲田さんの口座に100円入金する
+		nakata.deposit(100);
+		
+		System.out.println("■足立さんの口座");
+		
+		// 設計図クラスのメソッドの呼出
+		System.out.println("　口座名義 ： " + adachi.getName());
+		System.out.println("　口座番号 ： " + adachi.getNo());
+		System.out.println("　預金残高 ： " + adachi.getBalance());
+		
+		System.out.println("■仲田さんの口座");
+		
+		// 設計図クラスのメソッドの呼出
+		System.out.println("　口座名義 ： " + nakata.getName());
+		System.out.println("　口座番号 ： " + nakata.getNo());
+		System.out.println("　預金残高 ： " + nakata.getBalance());
+
+	}
+
+}
