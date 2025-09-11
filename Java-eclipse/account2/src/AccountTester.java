e4b27f324d7a4e33a2ebd59c003b91e4da9622c2
AB25 <AB25@PC127.fa.shiga-center.ac.jp>
2025-09-11 14:43:58 +0900

    commit

---
+++ Java-eclipse/account2/src/AccountTester.java
@@ -0,0 +1,104 @@
+// 銀行口座クラス【第2版】とテスト用クラス
+
+// 銀行口座クラス【第2版】
+// 設計図クラス
+class Account {
+	private String name;
+	private String no;
+	private long balance;
+	
+	// コンストラクタ(Accountクラスの初期化)
+	Account (String n, String num, long z) {
+		name = n;
+		no = num;
+		balance = z;
+	}
+	
+	// 口座名義を調べる
+	String getName() {
+		return name;
+	}
+	
+	// 口座番号を調べる
+	String getNo() {
+		return no;
+	}
+	
+	// 預金残高を調べる
+	long getBalance() {
+		return balance;
+	}
+	
+	// k円預ける
+	void deposit (long k) {
+		balance += k;
+	}
+	
+	// k円おろす
+	void withdraw (long k) {
+		balance -= k;
+	}
+	
+}
+
+// 銀行口座クラス【第2版】をテストするクラス
+// 実行用クラス
+public class AccountTester {
+
+	public static void main(String[] args) {
+		// 足立さんの口座
+		Account adachi = new Account("足立幸一", "123456", 1000);
+		// 仲田さんの口座
+		Account nakata = new Account("仲田真二", "654321", 200);
+		
+		// 足立さんの口座から200円おろす
+		adachi.withdraw(200);
+		// 仲田さんの口座に100円入金する
+		nakata.deposit(100);
+
+		
+		// コンストラクタを生成した場合、以下はコンパイルエラーとなる
+		
+		// コンストラクタを生成しているので、引数無しでインスタンスは作れない
+//		Account adachi = new Account();
+		
+//		// 足立さんの口座情報
+//		adachi.name = "足立幸一";
+//		adachi.no = "123456";
+//		adachi.balance = 1000;
+//		
+//		// 仲田さんの口座情報
+//		nakata.name = "仲田真二";
+//		nakata.no = "654321";
+//		nakata.balance = 200;
+		
+//		adachi.balance -= 200; // 足立さんの口座から200円おろす
+//		nakata.balance += 100; // 仲田さんの口座に100円入金する
+		
+		
+		
+		System.out.println("■足立さんの口座");
+		
+		// 設計図クラスのメソッドの呼出
+		System.out.println("　口座名義 ： " + adachi.getName());
+		System.out.println("　口座番号 ： " + adachi.getNo());
+		System.out.println("　預金残高 ： " + adachi.getBalance());
+		
+//		System.out.println("　口座名義 ： " + adachi.name);
+//		System.out.println("　口座番号 ： " + adachi.no);
+//		System.out.println("　預金残高 ： " + adachi.balance);
+		
+		System.out.println("■仲田さんの口座");
+		
+		// 設計図クラスのメソッドの呼出
+		System.out.println("　口座名義 ： " + nakata.getName());
+		System.out.println("　口座番号 ： " + nakata.getNo());
+		System.out.println("　預金残高 ： " + nakata.getBalance());
+		
+//		System.out.println("　口座名義 ： " + nakata.name);
+//		System.out.println("　口座番号 ： " + nakata.no);
+//		System.out.println("　預金残高 ： " + nakata.balance);
+
+	}
+
+}
--- Java-eclipse/chap08/src/AccountTester.java
+++ Java-eclipse/chap08/src/AccountTester.java
@@ -3,20 +3,44 @@
 // 銀行口座クラス【第一版】
 // 設計図クラス
 class Account {
+	// メンバ変数（static無しなので、インスタンス変数）
 	String name;  // 口座名義
 	String no;    // 口座番号
-	Long balance; // 口座名義
+	long balance; // 預金残高
 }
 
 // 銀行口座クラスをテストするクラス
 // 実行用クラス
 public class AccountTester {
-	Account adachi = new Account();
 	
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 足立さんの口座
+		Account adachi = new Account();
+		// 仲田さんの口座
+		Account nakata = new Account();
 
+		// 足立さんの口座情報
+		adachi.name = "足立幸一";
+		adachi.no = "123456";
+		adachi.balance = 1000;
+		
+		// 仲田さんの口座情報
+		nakata.name = "仲田真二";
+		nakata.no = "654321";
+		nakata.balance = 200;
+		
+		adachi.balance -= 200; // 足立さんの口座から200円おろす
+		nakata.balance += 100; // 仲田さんの口座に100円入金する
+		
+		System.out.println("■足立さんの口座");
+		System.out.println("　口座名義 ： " + adachi.name);
+		System.out.println("　口座番号 ： " + adachi.no);
+		System.out.println("　預金残高 ： " + adachi.balance);
+		
+		System.out.println("■仲田さんの口座");
+		System.out.println("　口座名義 ： " + nakata.name);
+		System.out.println("　口座番号 ： " + nakata.no);
+		System.out.println("　預金残高 ： " + nakata.balance);
 	}
 
 }
