ab7fc63d9cafaee16943b3fa470f476e996218a0
AB25 <AB25@PC127.fa.shiga-center.ac.jp>
2025-09-11 12:37:09 +0900

    commit

---
+++ Java-eclipse/chap08/src/AccountTester.java
@@ -0,0 +1,22 @@
+// 銀行口座クラス【第一版】とそれをテストするクラス
+
+// 銀行口座クラス【第一版】
+// 設計図クラス
+class Account {
+	String name;  // 口座名義
+	String no;    // 口座番号
+	Long balance; // 口座名義
+}
+
+// 銀行口座クラスをテストするクラス
+// 実行用クラス
+public class AccountTester {
+	Account adachi = new Account();
+	
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
