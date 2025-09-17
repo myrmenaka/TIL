b195b9ae0fc7128eb9acdf499f912befed99555c
AB25 <AB25@PC127.fa.shiga-center.ac.jp>
2025-09-17 15:04:07 +0900

    commit

--- Java-eclipse/reviewTest/src/MethodAdd.java
+++ Java-eclipse/reviewTest/src/MethodAdd.java
@@ -4,6 +4,10 @@ public class MethodAdd {
 	public static void main(String[] args) {
 		int sum = 0;
 		add(5, 10);
+		
+		// addメソッドの処理を実行する場合
+//		sum = add(5, 10);
+		
 		System.out.println(sum);
 
 	}
@@ -11,5 +15,11 @@ public class MethodAdd {
 	public static void add(int a, int b) {
 		int sum = a + b;
 	}
+	
+	// addメソッドの処理を実行する場合 
+//	public static int add(int a, int b) {
+//		int sum = a + b;
+//		return sum;
+//	}
 
 }
