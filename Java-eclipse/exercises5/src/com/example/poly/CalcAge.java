8d307c55730f14573686a776cf0042c608272d7e
AB25 <AB25@PC127.fa.shiga-center.ac.jp>
2025-09-24 13:55:53 +0900

    commit

--- Java-eclipse/exercises5/src/com/example/poly/CalcAge.java
+++ Java-eclipse/exercises5/src/com/example/poly/CalcAge.java
@@ -1,29 +1,43 @@
 // 年齢計算
 package com.example.poly;
 
+/*
+ * 実務でどのバージョンのjavaを使っているかにもよって、使えるクラスなど変わってくる
+ * バージョン管理をおろそかにすれば知っている古いクラスでしか対応できなくなる
+ * 状況に応じて知識を更新し、どのクラスを使うかはそのプログラマーの裁量によって異なる
+ */
 import java.util.*; // Scannerクラス
 import java.text.SimpleDateFormat;
 import java.time.*; // LocalDateTimeクラス
+import java.time.format.DateTimeFormatter;
 public class CalcAge {
 
 	public static void main(String[] args) {
-		CalcAge.getDate();
+		System.out.println("現在の日付：" + getDate());
+		System.out.println("現在の日付:" + getDateStr("yyyy年M月d日（E）"));
+		
 		System.out.print("生まれた西暦を入力してください：");
-		int birth = CalcAge.inputBirth();
-		int age = CalcAge.calcAgeNow(birth);
+		int birth = inputBirth();
+		
+		int age = calcAgeNow(birth);
 		System.out.println("あなたの年齢は" + age + "歳です");
 
 	}
 	
 	// 今日の日付を取得する
-	public static void getDate() {
+	public static String getDate() {
 		Date date = new Date();
 		// SimpleDateFomatクラスのインスタンス生成
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日(E)");
-		String text = sdf.format(date);
-		System.out.println("現在の日付：" + text);
+		return sdf.format(date);
 	}
 	
+	// LocalDateTimeバージョン java8から追加
+	public static String getDateStr(String format) {
+		LocalDateTime ldt = LocalDateTime.now();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+		return ldt.format(dtf);
+	}
 	
 	// ユーザーの生まれ年の入力
 	@SuppressWarnings("resource")
