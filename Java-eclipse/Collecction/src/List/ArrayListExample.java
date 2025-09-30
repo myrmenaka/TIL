d1c27adc117874be0e7b012d5d4dad3dd7e9105e
AB25 <AB25@PC127.fa.shiga-center.ac.jp>
2025-09-30 12:34:35 +0900

    commit

--- Java-eclipse/Collecction/src/List/ArrayListExample.java
+++ Java-eclipse/Collecction/src/List/ArrayListExample.java
@@ -4,23 +4,25 @@ import java.util.*;
 public class ArrayListExample {
 
 	public static void main(String[] args) {
-		// monthという可変長配列を生成
+		// ArrayListのインスタンスを生成（空）
 		ArrayList<String> month = new ArrayList<>();
 		
 		// addメソッドで"January"、"February"、"March"を要素の末尾に追加
-		month.add("January");
-		month.add("February");
-		month.add("March");
+		month.add("January"); // 0
+		month.add("February"); // 1
+		month.add("March"); // 2
 		
-		// sizeメソッドで要素数を取得して表示
+		// sizeメソッドで要素数を取得
 		System.out.println("要素数" + month.size());
 		
+		// 配列の走査・全要素の表示
 		// getメソッドで引数に渡された番号の要素を取得
 		for (int i = 0; i < month.size(); i++) {
 			System.out.println(month.get(i));
 		}
 		
 		// removeメソッドで引数番号の要素を削除
+		// 削除だけでなく、要素を詰めるまでしてくれる → NULLではないんだよ
 		month.remove(1);
 		
 		System.out.println("要素数" + month.size());
